a1211def97342588822b256529c540e3fa618ddf
theresaanna <theresa.summa@cfpb.gov>
2013-01-10 19:40:21 +0000

    moving into require.js modules

---
+++ scripts/cfpb-data-layouts.js
@@ -0,0 +1,6 @@
+define(["layouts/pie-chart"], function(pieChart) {
+    var DataVis = {};
+    DataVis.PieChart = pieChart;
+
+    return DataVis;
+});
---
+++ scripts/layouts/settings.js
@@ -0,0 +1,6 @@
+define(function() {
+    return {
+        colorGradient: ["#444", "#555", "#666", "#777", "#888", "#999", "#aaa"],
+        activeColor: "#50B948"
+    }
+});
