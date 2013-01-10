94fd0d8cdfd5234725db1b2cebd7210a40079913
theresaanna <theresa.summa@cfpb.gov>
2013-01-10 19:44:41 +0000

    removing old script

--- scripts/cfpb-data-vis.js
+++ scripts/cfpb-data-vis.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
     if (CFPBDATA) {
-console.log('hi');
         require(["libs/d3/d3.v3.min", "cfpb-data-layouts"], function(D3, DataVis) {
             var cfpbData = JSON.parse(CFPBDATA),
                 // in the future we'll need to determine vis type
-                pieChart = new DataVis.PieChart(cfpbData);
+                pieChart = DataVis.PieChart(cfpbData);
         pieChart.render(cfpbData);
         }); 
     }
--- scripts/layouts/settings.js
+++ scripts/layouts/settings.js
@@ -1,3 +1,4 @@
+// definitions of attributes that should apply to all layouts
 define(function() {
     return {
         colorGradient: ["#444", "#555", "#666", "#777", "#888", "#999", "#aaa"],
