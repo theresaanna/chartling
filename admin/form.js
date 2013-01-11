0d15ee02f691729d3aa99fd317b07a9cd7435501
theresaanna <theresa.summa@cfpb.gov>
2013-01-11 19:15:05 +0000

    form for pie chart json generation

--- admin/form.js
+++ admin/form.js
@@ -6,7 +6,30 @@ $(document).ready(function() {
         linkText: "Pie Chart"
     };
 
-    var layoutList = ich.linkList(CFPBData.layoutTypes);
+    // event handlers
+    var openForm = function(event) {
+        var formType = event.target.id.toString(),
+            templateName = formType + 'Form',
+            optionForm,
+            form = {};
 
+        form.options = [];
+
+        // temp solution
+        for (var i = 1; i <= 5; i++) {
+           form.options.push({num: i}); 
+        }
+
+        optionForm = ich[templateName](form);
+        $("#visualization-form").append(optionForm);
+        
+    };
+    
+    // render visualization options
+    var layoutList = ich.linkList(CFPBData.layoutTypes);
     $("#layout-types").append(layoutList);
+
+    // event bindings
+    $(".option-link").on('click', openForm);
+
 });
